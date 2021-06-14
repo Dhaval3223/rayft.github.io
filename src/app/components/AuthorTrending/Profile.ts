00c8b759773e961d0e87f97b46779d73411b155b
Dhaval3223 <mailto:dhavalmulashiya1234@gmail.com>
2021-06-14 13:28:52 +0530

    add Authortreanding

---
+++ src/app/components/AuthorTrending/Profile.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+
+export const Profile = styled.div`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: row;
+        min-height: 100px`;
