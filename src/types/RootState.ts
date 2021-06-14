88a87c2b9cbfec5e13aa731dca9365b04d1b4e3f
Dhaval3223 <mailto:dhavalmulashiya1234@gmail.com>
2021-06-14 13:31:33 +0530

    remove login module

--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -1,11 +1,10 @@
-import { LoginPageState } from 'app/pages/Login/slice/types';
+
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 
 /* 
   Because the redux-injectors injects your reducers asynchronously somewhere in your code
   You have to declare them here manually
 */
-export interface RootState {
-  loginpage?: LoginPageState;
+export interface RootState {  
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
 }
